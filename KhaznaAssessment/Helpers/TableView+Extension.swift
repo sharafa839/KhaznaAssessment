4977eb71a95af5f404c040582fa6958e93a3e779
Sharaf <sharafa839@gmail.com>
2024-06-22 03:09:01 +0300

    add table view extension

--- KhaznaAssessment/Helpers/TableView+Extension.swift
+++ KhaznaAssessment/Helpers/TableView+Extension.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITableView {
+    @discardableResult
+    func register<Cell: UITableViewCell> (nib cell: Cell.Type)-> Self {
+        self.register(cell.nib, forCellReuseIdentifier: cell.identifier)
+        return self
+    }
+    
+    @discardableResult
+    func dequeue<Cell: UITableViewCell>()-> Cell {
+        let id = Cell.identifier
+        guard let cell = self.dequeueReusableCell(withIdentifier: id) as? Cell else { 
+            fatalError("Cannot dequeue cell with identifier '\(id)'")
+        }
+        return cell
+    }
+}
+
+extension UIView {
+    static var identifier: String {
+        String(describing: self)
+    }
+    
+    static var nib: UINib {
+        UINib(nibName: "\(self)", bundle: nil)
+    }
+}
