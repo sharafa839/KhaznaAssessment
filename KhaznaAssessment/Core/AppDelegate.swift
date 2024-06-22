8cf105e75d2bd5b5d07b4fa460478b6ee9c3c5b7
Sharaf <sharafa839@gmail.com>
2024-06-22 03:00:08 +0300

    set app delegate

--- KhaznaAssessment/Core/AppDelegate.swift
+++ KhaznaAssessment/Core/AppDelegate.swift
@@ -8,11 +8,15 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+internal final class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    var window: UIWindow?
+    private(set) lazy var coordinator = AppCoordinator()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let splashViewController = SplashRouter.createModule()
+        coordinator.setRoot(splashViewController)
         return true
     }
 }
-
