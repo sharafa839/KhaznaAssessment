cfbd9bb07d3c1a1eb90e794e3f389156c622bc1b
Sharaf <sharafa839@gmail.com>
2024-06-22 03:09:16 +0300

    add data extension

--- KhaznaAssessment/Helpers/Data+Extension.swift
+++ KhaznaAssessment/Helpers/Data+Extension.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+extension Data {
+    
+    var prettyString: NSString? {
+        guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: .prettyPrinted),
+              let string = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
+        return string
+    }
+}
